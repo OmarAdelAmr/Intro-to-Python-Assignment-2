5853af2750e9b4dc89738e06cfcc8626ceede8a8
Omar Amr <omaradel9545@gmail.com>
2017-12-13 20:33:58 +0100

    [ADD] not much added

--- ex6.py
+++ ex6.py
@@ -1,8 +1,34 @@
 from ex5 import read_input_file
 
 
-def visualize_board():  # TODO
-    print("TODO")
+def switch_turn(turn):
+    if turn == 1:
+        return 2
+    else:
+        return 1
+
+
+def draw_board(rows, columns, p1_symbol, p2_symbol, game_entries):
+    vertical_bars = rows * len(p1_symbol) + (rows - 1)
+    print(game_entries)
+    board = ""
+
+    # row_separation_counter = 0
+    # for counter in range(vertical_bars):
+    #
+    #     line = "|," * (columns + 1)
+    #     line = line[:len(line) - 1]
+    #     if row_separation_counter == symbol_size:
+    #         line = line.replace(",", "-" * symbol_size)
+    #         row_separation_counter = 0
+    #     else:
+    #         line = line.replace(",", " " * symbol_size)
+    #         row_separation_counter += 1
+    #     board += "\n" + line
+    #
+    # board += "\n" + "-" * (symbol_size * columns + (columns + 1))
+    #
+    # return board
 
 
 def play(config_file):
@@ -13,7 +39,7 @@ def play(config_file):
     columns_number = board_configuration[3]
     rows_number = board_configuration[4]
 
-    print(board)
+    print(p1_symbol)
 
     game_over = False
     p1_won = False
@@ -30,12 +56,18 @@ def play(config_file):
             try:
                 empty_rows_in_column = -1
                 input_column = int(input("Player's {} turn: ".format(turn)))
-                print(input_column)  # check index inside boundaries
+                if input_column < 1 or input_column > columns_number:
+                    empty_rows_in_column = -2
+                    raise ValueError
                 empty_rows_in_column = game_entries[input_column - 1].index("NULL")
                 game_entries[input_column - 1][empty_rows_in_column] = turn
+                turn = switch_turn(turn)
+                draw_board(rows_number, columns_number, p1_symbol, p2_symbol, game_entries)
             except ValueError:
                 if empty_rows_in_column == -1:
-                    print("Column {} has no empty space.")
+                    print("Column {} has no empty space.".format(input_column))
+                elif empty_rows_in_column == -2:
+                    print("Please enter a valid column number.")
                 else:
                     print("Please enter a numeric value.")
 
