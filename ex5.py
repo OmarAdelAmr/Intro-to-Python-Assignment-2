2da76b0b4b80bfdef71e0849c6c419b2c848019b
Omar Amr <omaradel9545@gmail.com>
2017-12-17 00:49:59 +0100

    [ADD] ex 6 done, tested but not so much.

--- ex5.py
+++ ex5.py
@@ -10,8 +10,8 @@ def get_content_between_quotations(input_string):
 def validate_players_symbol(symbol1, symbol2):
     symbol1_rows = len(symbol1)
     symbol2_rows = len(symbol2)
-
     valid = True
+
     if symbol1_rows != symbol2_rows:
         valid = False
 
@@ -22,36 +22,33 @@ def validate_players_symbol(symbol1, symbol2):
         if len(x) != symbol1_rows:
             valid = False
 
-    if not valid:
-        print("Players' symbols are not valid")
-        return
+    return valid
 
 
 def draw_board(rows, columns, symbol_size):
-    vertical_bars = rows * symbol_size + (rows - 1)
-
-    board = []
-
-    row_separation_counter = 0
-    for counter in range(vertical_bars):
-
-        line = "|," * (columns + 1)
-        line = line[:len(line) - 1]
-        if row_separation_counter == symbol_size:
-            line = line.replace(",", "-" * symbol_size)
-            row_separation_counter = 0
-        else:
-            line = line.replace(",", " " * symbol_size)
-            row_separation_counter += 1
-
-        board.append(line)
-
-    board.append("-" * (symbol_size * columns + (columns + 1)))
-
-    for line in board:
-        print(line)
+    line = " " * columns * symbol_size + "\n"
+    line = line * rows * symbol_size
+    board = line.split("\n")
+    board = [x for x in board if x != ""]
 
     return board
+
+
+def visualize_board(board, columns, symbol_size):
+    result = ""
+    separator_counter = 0
+    for line in board:
+        result += "|" + '|'.join(line[i:i + symbol_size] for i in range(0, len(line) + 1, symbol_size)) + "\n"
+        separator_counter += 1
+        if separator_counter == symbol_size:
+            separator = "|" * (columns + 1)
+            separator = ('-' * symbol_size).join(separator[i:i + 1] for i in range(0, len(separator), 1)) + "\n"
+            result += separator
+            separator_counter = 0
+    result = result[:(result[:result.rfind('\n')]).rfind('\n')]  # remove last 2 lines
+    result += "\n" + "-" * (columns * symbol_size + columns + 1)
+
+    print(result)
 
 
 def read_input_file(file):
@@ -77,8 +74,12 @@ def read_input_file(file):
             p2_symbol = x.split("=")[1].split(",")
             p2_symbol = [get_content_between_quotations(item) for item in p2_symbol]
 
-    validate_players_symbol(p1_symbol, p2_symbol)
+    if not validate_players_symbol(p1_symbol, p2_symbol):
+        print("Players' symbols are not valid")
+        return
+
     board = draw_board(rows, columns, len(p1_symbol))
+    visualize_board(board, columns, len(p1_symbol))
 
     return board, p1_symbol, p2_symbol, columns, rows
 
@@ -86,6 +87,3 @@ def read_input_file(file):
 if __name__ == "__main__":
     file_name = "example.config"
     playing_board = read_input_file(file_name)
-
-# TO READ: Solution is to make board only made of entries (spaces or symbols) and bars or separating lines
-# drawn in a visualization function
--- ex6.py
+++ ex6.py
@@ -1,4 +1,5 @@
 from ex5 import read_input_file
+from ex5 import visualize_board
 import numpy as np
 
 
@@ -9,95 +10,91 @@ def switch_turn(turn):
         return 1
 
 
-def draw_board(rows, columns, p1_symbol, p2_symbol, game_entries):
-    vertical_bars = rows * len(p1_symbol) + (rows - 1)
-    symbol_size = len(p1_symbol)
+def check_if_successive(item):
+    if len(item) < 4:
+        return False
 
-    game_entries = [list(x) for x in game_entries]
-    game_entries = np.flip(np.transpose(game_entries), 0)
-    # print(game_entries[0])
-
-    # xx = [game_entries.x for x in game_entries[4] if x != "NULL"]
-    xxx = game_entries[4][game_entries[4] != "NULL"]
-    xxx = np.nonzero(game_entries[4] != "NULL")
-    print(xxx[0][0])
-    # print("-->", xxx)
-
-    board = ""
-
-    for row_counter in range(rows):
-        non_empty_values = np.nonzero(game_entries[row_counter] != "NULL")
-        non_empty_indices = np.nonzero(game_entries[row_counter] != "NULL")
-        print(non_empty_indices)
-        current_row_counter = 0
-
-        for counter in range(symbol_size):
-            line = "|," * (columns + 1)
-            line = line[:len(line) - 1]
-            current_row_counter += 1
-
-            # if game_entries[row_counter][column_counter] != "NULL":
-
-            # print(game_entries[row_counter][column_counter])
-
-        line = "|," * (columns + 1)
-        line = line[:len(line) - 1]
-        if current_row_counter == symbol_size:
-            line = line.replace(",", "-" * symbol_size)
+    counter = 1
+    for x in range(1, len(item)):
+        if item[x] - item[x - 1] == 1:
+            counter += 1
+            if counter == 4:
+                return True
         else:
-            line = line.replace(",", " " * symbol_size)
+            counter = 1
 
-        board += "\n" + line
-
-    board += "\n" + "-" * (symbol_size * columns + (columns + 1))
-
-    return board
+    return False
 
 
-def insert_bars(l):
-    result = ["|"] * (len(l) * 2)
-    result[0::2] = l
-    result = "|" + "".join(str(x) for x in result)
-    return result
+def check_game_status(board, columns, rows, turn):
+    turn = str(turn)
+    board1 = np.flip(np.transpose(board), 0)
+    board2 = np.flip(board1, -1)
+    status = [False, False, False]  # [Game over, P1 won, P2 won]
+
+    for column in np.array(board):
+        x = np.nonzero(column == turn)[0]
+        print(x)
+        if check_if_successive(x):
+            status[int(turn)] = True
+            return status
+
+    for row in board1:
+        x = np.nonzero(row == turn)[0]
+        if check_if_successive(x):
+            status[int(turn)] = True
+            return status
+
+    for counter in range(-1 * rows + 1, columns):
+        diagonal1 = np.diag(board1, counter)
+        diagonal2 = np.diag(board2, counter)
+        x = np.nonzero(diagonal1 == turn)[0]
+        y = np.nonzero(diagonal2 == turn)[0]
+        if check_if_successive(x) or check_if_successive(y):
+            status[int(turn)] = True
+            return status
+
+    empty_counter = 0
+    for column in board:  # Check of all entries are full
+        empty = [e for e in column if e == "NULL"]
+        empty_counter += len(empty)
+
+    if empty_counter == 0:
+        status[0] = True
+        return status
+
+    return status
 
 
-def visualize_board(board):
-    for line in board:
-        print(line)
+def draw_board(game_entries, columns_number, p1_symbol, p2_symbol, symbol_size):
+    game_entries = np.flip(np.transpose(game_entries), 0)
+    visualization_board = []
 
+    for row in game_entries:
+        for counter in range(symbol_size):
+            line = ""
+            for item in row:
+                if item != "NULL":
+                    if item == '1':
+                        line += p1_symbol[counter]
+                    else:
+                        line += p2_symbol[counter]
+                else:
+                    line += " " * symbol_size
 
-def draw_board2(column, row, symbol, board, height):
-    symbol_size = len(symbol)
+            visualization_board.append(line)
 
-    start_index = height - (row * symbol_size + row)
-
-    change_rows = range(start_index, start_index + symbol_size)
-
-    for x in change_rows:
-        r = board[height - x]
-        temp_symbol = symbol[x % symbol_size]
-        r = r.split("|")
-        r = r[1:len(r) - 1]
-        r[symbol_size] = temp_symbol
-        r = insert_bars(r)
-        r = "".join(str(x) for x in r)
-        board[height - x] = r
-
-    visualize_board(board)
-
-    return board
-    print("TODO")
+    visualize_board(visualization_board, columns_number, symbol_size)
 
 
 def play(config_file):
     board, p1_symbol, p2_symbol, columns_number, rows_number = read_input_file(config_file)
-    # print(board)
     game_over, p1_won, p2_won = [False, False, False]
     turn = 1
 
     game_entries = [["NULL" for x in range(rows_number)] for y in range(columns_number)]  # array list of each column
 
-    while not (game_over and p1_won and p2_won):
+    while not (game_over or p1_won or p2_won):
         valid_column = False
         while not valid_column:
             try:
@@ -107,14 +104,12 @@ def play(config_file):
                     empty_rows_in_column = -2
                     raise ValueError
                 empty_rows_in_column = game_entries[input_column - 1].index("NULL")
-                game_entries[input_column - 1][empty_rows_in_column] = turn
+                game_entries[input_column - 1][empty_rows_in_column] = str(turn)
+                valid_column = True
+                draw_board(game_entries, columns_number, p1_symbol, p2_symbol, len(p1_symbol))
+                p1_won, p2_won, game_over = check_game_status(game_entries, columns_number, rows_number, turn)
                 turn = switch_turn(turn)
-                # print(game_entries)
-                # print(input_column - 1)
-                # print(empty_rows_in_column)
-                # print("row", empty_rows_in_column)
-                board = draw_board2(input_column - 1, empty_rows_in_column, p1_symbol, board, columns_number)
-                # draw_board(rows_number, columns_number, p1_symbol, p2_symbol, game_entries)
+
             except ValueError:
                 if empty_rows_in_column == -1:
                     print("Column {} has no empty space.".format(input_column))
@@ -123,9 +118,12 @@ def play(config_file):
                 else:
                     print("Please enter a numeric value.")
 
-        print("outside inner loop")
-
-    # print(board)
+    if game_over:
+        print("Game over, It's a tie.")
+    elif p1_won:
+        print("Player 1 won.")
+    else:
+        print("Player 2 won.")
 
 
 if __name__ == "__main__":
